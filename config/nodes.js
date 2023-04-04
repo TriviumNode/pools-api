1a240a047176856617cf816a6d422f8831a4ffef
Xiphiar <me@xiphiar.com>
2023-04-04 00:23:08 +0000

    meh

--- config/nodes.js
+++ config/nodes.js
@@ -3,19 +3,27 @@ const controlNodeSecret = 'https://rpc.scrt.network/status';
 const controlNodePulsar = 'http://40.88.137.151:26657/status';
   
 const triviumNodes = [
-    // 'http://66.85.137.179:26657/status', // tpn02-node01
-    // 'http://66.85.137.180:26657/status', // tpn02-node02
+    'http://66.85.142.171:26657/status', // tpn01-node01
+    'http://66.85.142.172:26657/status', // tpn01-node02
+
+    'http://66.85.142.146:26657/status', // phx02-node01
+    'http://66.85.142.147:26657/status', // phx02-node02
 
     'http://174.138.172.51:26657/status', // anode01-node01
     'http://174.138.172.52:26657/status', // anode01-node01
 
     'http://131.153.57.226:26657/status', // chi-node01
     'http://131.153.57.227:26657/status', // chi-node02
-    'http://131.153.57.228:26657/status', // chi-node03
+    // 'http://131.153.57.228:26657/status', // chi-node03
 
     'http://131.153.175.90:26657/status', // ash-node01
     'http://131.153.175.91:26657/status', // ash-node02
-    'http://131.153.175.92:26657/status', // ash-node03
+    // 'http://131.153.175.92:26657/status', // ash-node03
+    
+    'http://131.153.174.10:26657/status', // ash2-node01
+    'http://131.153.174.11:26657/status', // ash2-node02
+
+
 ];
 
 const pulsarNodes = [
